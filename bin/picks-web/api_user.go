03ba530a46cc1a8a7122f7829ad1cad00092171f
jake <bigjake2s@gmail.com>
2014-11-17 06:27:40 -0500

    Added user handling via API and storage funcs

--- bin/picks-web/api_user.go
+++ bin/picks-web/api_user.go
@@ -1,15 +1,24 @@
 package main
 
-type User struct{}
+import (
+	"github.com/jbaikge/nfl-picks/bin/picks-web/apitypes"
+	"log"
+)
 
-type UserAuthOut struct {
-	Valid bool
-}
+type User struct{}
 
 func init() {
 	RegisterAPI(new(User))
 }
 
-func (api *User) Auth(in *Nil, out *Nil) (err error) {
+func (api *User) Auth(in *apitypes.UserAuthIn, out *apitypes.UserAuthOut) (err error) {
+	log.Printf("Username: `%s', PIN: `%s'", in.Username, in.PIN)
+	out.Id, out.IsAdmin, err = Store.UserValidate(in.Username, in.PIN)
+	out.Username = in.Username
+	return
+}
+
+func (api *User) Usernames(in *Nil, out *apitypes.UserUsernamesOut) (err error) {
+	out.Usernames, err = Store.Usernames()
 	return
 }
--- bin/picks-web/apitypes/apitypes.go
+++ bin/picks-web/apitypes/apitypes.go
@@ -30,3 +30,18 @@ type PicksSubmitOut struct {
 	Valid []bool
 	Saved bool
 }
+
+type UserAuthIn struct {
+	Username string
+	PIN      int
+}
+
+type UserAuthOut struct {
+	Id       int64
+	IsAdmin  bool
+	Username string
+}
+
+type UserUsernamesOut struct {
+	Usernames []string
+}
