1466fa714c5d68bda354ba9232752afe8dbc13ca
jake <bigjake2s@gmail.com>
2014-11-18 13:44:19 -0500

    Initial layout for picks page

--- assets/app/picks/submit/submit.module.js
+++ assets/app/picks/submit/submit.module.js
@@ -1,37 +1,38 @@
 "use strict"
 
-angular.module("Picks.User.Pick", [
+angular.module("Picks.Picks.Submit", [
 	"ngRoute"
 ])
 
 .config(["$routeProvider", function($routeProvider) {
-	$routeProvider.when("/user/pick", {
-		controller:  "Picks.User.PickController",
-		templateUrl: "/app/user/pick/pick.partial.html",
+	$routeProvider.when("/picks/submit", {
+		controller:  "Picks.Picks.SubmitController",
+		templateUrl: "/app/picks/submit/submit.partial.html",
 		auth:        function(user) { return angular.isDefined(user) }
 	})
 }])
 
 // pick.service.js
-angular.module("Picks.User.Pick").service("PickService", ["jsonrpc", function(jsonrpc) {
-	this.currentGames = function() {
-		return jsonrpc("Game.CurrentGames")
+angular.module("Picks.Picks.Submit").service("SubmitService", ["jsonrpc", function(jsonrpc) {
+	this.currentLines = function() {
+		return jsonrpc("Lines.Current")
 	}
 }])
 
 // pick.controller.js
-angular.module("Picks.User.Pick").controller("Picks.User.PickController", [
+angular.module("Picks.Picks.Submit").controller("Picks.Picks.SubmitController", [
 	"$scope",
-	"PickService",
-	function($scope, PickService) {
-		$scope.Games = []
+	"SubmitService",
+	function($scope, SubmitService) {
+		$scope.Lines = []
 
-		PickService.currentGames()
+		SubmitService.currentLines()
 			.success(function(data, status) {
-				$scope.Games = data.result.Games
+				$scope.Current = data.result.Current
+				$scope.Lines = data.result.Lines
 			})
 			.error(function(data, status) {
-				console.log("PickService", "status", status, "data", data)
+				console.log("SubmitService", "status", status, "data", data)
 			})
 	}
 ])
