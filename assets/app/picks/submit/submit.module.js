6cd7ac967b2b7597dcd6dbe17dff4da7bded15da
jake <bigjake2s@gmail.com>
2014-11-19 15:25:04 -0500

    Styling and submission updates for pick submission

--- assets/app/picks/submit/submit.module.js
+++ assets/app/picks/submit/submit.module.js
@@ -36,6 +36,48 @@ angular.module("Picks.Picks.Submit").controller("Picks.Picks.SubmitController", 
 	function($rootScope, $scope, SubmitService) {
 		$scope.Lines = []
 		$scope.Picks = {}
+		$scope.Progress = {}
+
+		$scope.$watch("Picks", function(newValue, oldValue) {
+			var o = { Home: 0, Away: 0, Over: 0, Under: 0 }
+			var changed = []
+			for (var id in newValue) {
+				var value = newValue[id]
+				if (!oldValue.hasOwnProperty(id) || oldValue[id] != value) {
+					changed.push({ GameId: id, Value:  value })
+				}
+				switch (true) {
+				case value == "OVER":        o.Over++;  break
+				case value == "UNDER":       o.Under++; break
+				case id.indexOf(value) == 0: o.Away++;  break
+				case id.indexOf(value) > 0:  o.Home++;  break
+				}
+			}
+			// Fix up the percentages
+			var total = 0
+			for (var k in o) {
+				o[k] = Math.round(o[k] / $scope.Lines.length * 100)
+				total += o[k]
+			}
+			if (total > 100) {
+				o.Under -= total - 100
+			}
+			console.log("counts:", o, "changed:", changed)
+
+			if (newValue.initial) {
+				delete($scope.Picks.initial)
+				return
+			}
+
+			SubmitService.submit($rootScope.User.Id, changed)
+				.success(function(data, status) {
+					console.log("SubmitService", "data", data)
+				})
+				.error(function(data, status) {
+					console.log("SubmitService.submit", "status", status, "data", data)
+				})
+			$scope.Progress = o
+		}, true)
 
 		$scope.submitPicks = function() {
 			var picks = []
@@ -63,9 +105,14 @@ angular.module("Picks.Picks.Submit").controller("Picks.Picks.SubmitController", 
 				if (picks == null) {
 					return
 				}
-				for (var i = 0; i < picks.length; i++) {
-					$scope.Picks[picks[i].GameId] = picks[i].Value
+				var myPicks = {
+					// Since this is the initial load, don't let @watch re-send the existing picks
+					initial: true
 				}
+				for (var i = 0; i < picks.length; i++) {
+					myPicks[picks[i].GameId] = picks[i].Value
+				}
+				$scope.Picks = myPicks
 			})
 			.error(function(data, status) {
 				console.log("SubmitService.currentLines", "status", status, "data", data)
