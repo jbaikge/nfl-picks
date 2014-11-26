8c70b3437d1a37619f4007ff099ebbcaefe1f954
jake <bigjake2s@gmail.com>
2014-11-26 06:55:54 -0500

    Corrected ID generation for iron workers

--- iron_workers/UpdateScores.sh
+++ iron_workers/UpdateScores.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ID=UpdateScores.`date`
-curl -XPOST -H'Content-Type: application/json' http://picks.keyboardfu.com/rpc -d '{ "id":"'$ID'", "method": "Game.UpdateScores", "params": [] }'
+ID=UpdateScores.`date +%Y-%m-%dT%T%:z`
+curl -XPOST -H'Content-Type: application/json' http://picks.keyboardfu.com/rpc -d '{ "id":"'"$ID"'", "method": "Game.UpdateScores", "params": [] }'
--- iron_workers/UpdateWeek.sh
+++ iron_workers/UpdateWeek.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ID=UpdateWeek.`date`
-curl -XPOST -H'Content-Type: application/json' http://picks.keyboardfu.com/rpc -d '{ "id":"'$ID'", "method": "Game.UpdateCurrentWeek", "params": [] }'
+ID=UpdateWeek.`date +%Y-%m-%dT%T%:z`
+curl -XPOST -H'Content-Type: application/json' http://picks.keyboardfu.com/rpc -d '{ "id":"'"$ID"'", "method": "Game.UpdateCurrentWeek", "params": [] }'
